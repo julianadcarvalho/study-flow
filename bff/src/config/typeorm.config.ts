34a99353e8b437ac45f02bafd7492f304b802044
julianadcarvalho <julianafsilvaju@gmail.com>
2025-04-15 22:13:32 -0300

    fix: update database configuration to use environment variables and create .env file in workflows

--- bff/src/config/typeorm.config.ts
+++ bff/src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'study-flow-mysql',
-  port: 3307,
-  username: 'root',
-  password: 'root',
-  database: 'study_flow',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: ['./../**/*.entity.js'],
   migrations: ['./../migrations/*.js'],
   synchronize: false,
